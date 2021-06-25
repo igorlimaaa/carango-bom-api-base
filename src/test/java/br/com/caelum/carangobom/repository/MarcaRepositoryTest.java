0e58419f19b9d468c0796a4f68dc0502d2b112c3
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-06-25 20:53:43 -0300

    Fix build

--- src/test/java/br/com/caelum/carangobom/repository/MarcaRepositoryTest.java
+++ src/test/java/br/com/caelum/carangobom/repository/MarcaRepositoryTest.java
@@ -1,20 +1,18 @@
 package br.com.caelum.carangobom.repository;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.caelum.carangobom.domain.Marca;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class MarcaRepositoryTest {
@@ -26,7 +24,7 @@ public class MarcaRepositoryTest {
 	public void deveRetornarListaMarcas() {
 		List<Marca> listMarca = marcaRepository.findByIdOrderNome();
 		boolean result = listMarca != null && !listMarca.isEmpty() ? true : false;
-		Assert.assertTrue(result);
+		assertTrue(result);
 	}
 
 }
