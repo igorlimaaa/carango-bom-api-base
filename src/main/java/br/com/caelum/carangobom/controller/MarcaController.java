6ac725aa57d9b765c7f3cd4fff3374fa356b6a94
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-06-25 19:11:10 -0300

    Correção nas classes de teste

--- src/main/java/br/com/caelum/carangobom/controller/MarcaController.java
+++ src/main/java/br/com/caelum/carangobom/controller/MarcaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -45,8 +44,13 @@ public class MarcaController {
     @GetMapping("/marcas/{id}")
     @ResponseBody
     @Transactional
-    public ResponseEntity<Marca> id(@PathVariable Long id) throws ObjectNotFoundException{
-        return new ResponseEntity<Marca>(marcaService.findByIdBrand(id), null, HttpStatus.OK);
+    public ResponseEntity<Marca> id(@PathVariable Long id){
+    	Marca marca = marcaService.findByIdBrand(id);
+    	if(marca != null) {
+    		return new ResponseEntity<Marca>(marca, null, HttpStatus.OK);
+    	}else {
+    		return new ResponseEntity<Marca>(marca, null, HttpStatus.NOT_FOUND);    		
+    	}
     }
     
 
@@ -55,26 +59,36 @@ public class MarcaController {
     @Transactional
     @CacheEvict(value = "listaMarcas", allEntries = true)
     public ResponseEntity<MarcaForm> cadastra(@Valid @RequestBody MarcaForm marca) {
-    	return new ResponseEntity<MarcaForm>(marcaService.saveBrand(marca), HttpStatus.OK);
+    	return new ResponseEntity<MarcaForm>(marcaService.saveBrand(marca), HttpStatus.CREATED);
     }
 
     @PutMapping("/marcas/{id}")
     @ResponseBody
     @Transactional
     @CacheEvict(value = "listaMarcas", allEntries = true)
-    public ResponseEntity<MarcaForm> altera(@PathVariable Long id, @Valid @RequestBody MarcaForm m1) throws ObjectNotFoundException{
-    	return new ResponseEntity<MarcaForm>(marcaService.updateBrand(id, m1), null, HttpStatus.OK);
+    public ResponseEntity<MarcaForm> altera(@PathVariable Long id, @Valid @RequestBody MarcaForm m1){
+    	MarcaForm marca = marcaService.updateBrand(id, m1);
+    	if(marca != null) {
+    		return new ResponseEntity<MarcaForm>(marca, null, HttpStatus.OK);
+    	}else {
+    		return new ResponseEntity<MarcaForm>(marca, null, HttpStatus.NOT_FOUND);    		
+    	}
     }
 
-    @DeleteMapping("/marcas/{id}")
-    @ResponseBody
-    @Transactional
-    @Validated
-    @CacheEvict(value = "listaMarcas", allEntries = true)
-    public ResponseEntity<Marca> deleta(@PathVariable Long id) throws ObjectNotFoundException {
-    	return new ResponseEntity<Marca>(marcaService.removeBrand(id), null, HttpStatus.OK);
-        
-    }
+	@DeleteMapping("/marcas/{id}")
+	@ResponseBody
+	@Transactional
+	@Validated
+	@CacheEvict(value = "listaMarcas", allEntries = true)
+	public ResponseEntity<Marca> deleta(@PathVariable Long id) {
+		Marca marca = marcaService.removeBrand(id);
+		if (marca != null) {
+			return new ResponseEntity<Marca>(marca, null, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<Marca>(marca, null, HttpStatus.NOT_FOUND);			
+		}
+
+	}
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
