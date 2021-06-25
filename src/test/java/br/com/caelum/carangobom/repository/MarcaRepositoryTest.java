5bd40ab3cfd09312b2f766763611e87291f10550
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-06-25 14:42:31 -0300

    Ajustes para criar um banco de dados em memória para os testes

--- src/test/java/br/com/caelum/carangobom/repository/MarcaRepositoryTest.java
+++ src/test/java/br/com/caelum/carangobom/repository/MarcaRepositoryTest.java
@@ -1,35 +1,29 @@
 package br.com.caelum.carangobom.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.caelum.carangobom.domain.Marca;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class MarcaRepositoryTest {
 	
 	@Autowired
 	private MarcaRepository marcaRepository;
 	
-	private void _configureListMarca() {
-		List<Marca> listMarca = new ArrayList<>();
-		listMarca.add(new Marca("Audi"));
-		listMarca.add(new Marca("Ford"));
-		listMarca.add(new Marca("Hyndai"));
-		marcaRepository.saveAll(listMarca);
-	}
-	
 	@Test
 	public void deveRetornarListaMarcas() {
-		_configureListMarca();
 		List<Marca> listMarca = marcaRepository.findByIdOrderNome();
 		boolean result = listMarca != null && !listMarca.isEmpty() ? true : false;
 		Assert.assertTrue(result);
