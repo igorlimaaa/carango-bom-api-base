91572634259e0bb91c2910211ce586110e99b949
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-07-01 17:18:18 -0300

    Fix Duplicated lines in MarcaForm

--- src/main/java/br/com/caelum/carangobom/form/MarcaForm.java
+++ src/main/java/br/com/caelum/carangobom/form/MarcaForm.java
@@ -6,48 +6,30 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import br.com.caelum.carangobom.domain.Marca;
+import lombok.Getter;
+import lombok.Setter;
 
 @Component
+@Getter
+@Setter
 public class MarcaForm {
-	
+
 	private Long id;
 	private String nome;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public List<MarcaForm> convertDomainToDto(List<Marca> marca) {
 		List<MarcaForm> listaType = new ArrayList<>();
-		marca.forEach(marcaDomain -> {
-			MarcaForm marcaType = new MarcaForm();
-			marcaType.setId(marcaDomain.getId());
-			marcaType.setNome(marcaDomain.getNome());
-			listaType.add(marcaType);
-		});
-		
+		marca.forEach(marcaDomain -> listaType.add(convertDomainToType(marcaDomain)));
 		return listaType;
 	}
-	
+
 	public Marca convertTypeToDomain(MarcaForm marcaType) {
 		Marca marcaDomain = new Marca();
 		marcaDomain.setId(null);
 		marcaDomain.setNome(marcaType.getNome());
 		return marcaDomain;
 	}
-	
+
 	public MarcaForm convertDomainToType(Marca marcaDomain) {
 		MarcaForm marcaType = new MarcaForm();
 		marcaType.setId(marcaDomain.getId());
--- src/main/java/br/com/caelum/carangobom/form/VeiculoForm.java
+++ src/main/java/br/com/caelum/carangobom/form/VeiculoForm.java
@@ -14,57 +14,54 @@ import lombok.Setter;
 @Getter
 @Setter
 public class VeiculoForm {
-	    
-	
-		private Long id;
-	    
-		private String modelo;
-	    
-	    private Long ano;
-	    
-	    private Double preco;
-	    
-	    private Boolean isVendido; 
-	    
-	    private MarcaForm marca;		
-		
-		public List<VeiculoForm> convertDomainToDto(List<Veiculo> veiculo) {
-			List<VeiculoForm> listaType = new ArrayList<>();
-			
-			veiculo.forEach(veiculoDomain -> listaType.add(convertDomainToType(veiculoDomain)));
-			
-			return listaType;
-		}
-		
-		public VeiculoForm convertDomainToType(Veiculo veiculoDomain) {
-			VeiculoForm veiculoType = new VeiculoForm();
-			veiculoType.setId(veiculoDomain.getId());
-			veiculoType.setAno(veiculoDomain.getAno());
-			veiculoType.setModelo(veiculoDomain.getModelo());
-			veiculoType.setPreco(veiculoDomain.getPreco());
-			veiculoType.setIsVendido(veiculoDomain.getIsVendido());
-			
-			MarcaForm marcaDomain = new MarcaForm();
-			marcaDomain.setId(veiculoDomain.getMarca().getId());
-			marcaDomain.setNome(veiculoDomain.getMarca().getNome());
-			veiculoType.setMarca(marcaDomain);
-			
-			return veiculoType;
-		}
 
-		public Veiculo convertTypeToDomain(VeiculoForm veiculoType) {
-			Veiculo veiculoDomain = new Veiculo();
-			veiculoDomain.setId(veiculoType.getId());
-			veiculoDomain.setAno(veiculoType.getAno());
-			veiculoDomain.setModelo(veiculoType.getModelo());
-			veiculoDomain.setPreco(veiculoType.getPreco());
-			veiculoDomain.setIsVendido(veiculoType.getIsVendido());
-			
-			Marca marcaDomain = new Marca();
-			marcaDomain.setId(veiculoType.getMarca().getId());
-			veiculoDomain.setMarca(marcaDomain);
-			
-			return veiculoDomain;
-		}
-		
+	private Long id;
+
+	private String modelo;
+
+	private Long ano;
+
+	private Double preco;
+
+	private Boolean isVendido;
+
+	private MarcaForm marca;
+
+	public List<VeiculoForm> convertDomainToDto(List<Veiculo> veiculo) {
+		List<VeiculoForm> listaType = new ArrayList<>();
+		veiculo.forEach(veiculoDomain -> listaType.add(convertDomainToType(veiculoDomain)));
+		return listaType;
+	}
+
+	public VeiculoForm convertDomainToType(Veiculo veiculoDomain) {
+		VeiculoForm veiculoType = new VeiculoForm();
+		veiculoType.setId(veiculoDomain.getId());
+		veiculoType.setAno(veiculoDomain.getAno());
+		veiculoType.setModelo(veiculoDomain.getModelo());
+		veiculoType.setPreco(veiculoDomain.getPreco());
+		veiculoType.setIsVendido(veiculoDomain.getIsVendido());
+
+		MarcaForm marcaDomain = new MarcaForm();
+		marcaDomain.setId(veiculoDomain.getMarca().getId());
+		marcaDomain.setNome(veiculoDomain.getMarca().getNome());
+		veiculoType.setMarca(marcaDomain);
+
+		return veiculoType;
+	}
+
+	public Veiculo convertTypeToDomain(VeiculoForm veiculoType) {
+		Veiculo veiculoDomain = new Veiculo();
+		veiculoDomain.setId(veiculoType.getId());
+		veiculoDomain.setAno(veiculoType.getAno());
+		veiculoDomain.setModelo(veiculoType.getModelo());
+		veiculoDomain.setPreco(veiculoType.getPreco());
+		veiculoDomain.setIsVendido(veiculoType.getIsVendido());
+
+		Marca marcaDomain = new Marca();
+		marcaDomain.setId(veiculoType.getMarca().getId());
+		veiculoDomain.setMarca(marcaDomain);
+
+		return veiculoDomain;
+	}
+
 }
