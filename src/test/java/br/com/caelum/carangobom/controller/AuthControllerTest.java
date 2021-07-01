0e6da43923f8c290f6183358d5d3a5b2bc09e86b
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-07-01 11:31:50 -0300

    Fix Teste

--- src/test/java/br/com/caelum/carangobom/controller/AuthControllerTest.java
+++ src/test/java/br/com/caelum/carangobom/controller/AuthControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @ActiveProfiles("test")
 public class AuthControllerTest {
 	
+	
 	@Autowired
 	private MockMvc mockMvc;
 
