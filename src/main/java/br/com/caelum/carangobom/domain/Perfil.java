6f55e17d16d48820476ce420f70b3866457b44ed
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-07-01 17:27:11 -0300

    Fix Duplicated lines in Perfik

--- src/main/java/br/com/caelum/carangobom/domain/Perfil.java
+++ src/main/java/br/com/caelum/carangobom/domain/Perfil.java
@@ -4,36 +4,25 @@ import javax.persistence.*;
 
 import org.springframework.security.core.GrantedAuthority;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
-@Table(name = "perfil")
+@Getter
+@Setter
+@Table(name = "tb_perfil")
 public class Perfil implements GrantedAuthority {
 	
 	private static final long serialVersionUID = 1L;
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id_perfir;
 	
-	private String nome;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+	private String ds_nome;
 
 	@Override
 	public String getAuthority() {
-		return nome;
+		return ds_nome;
 	}
 	
 }
