658a192ca36a98912aff4178dff7f051f58951ba
Bruno Vinicius Felix Da Silva <brunoviniciusfelixdasilva@MacBook-Pro-de-Bruno.local>
2021-07-01 15:16:36 -0300

    Implements Test Car

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,8 @@ INSERT INTO TB_MARCA (ds_nome) VALUES ('For');
 INSERT INTO TB_MARCA (ds_nome) VALUES ('HyndaI');
 
 -- Insert para Veículos
-INSERT INTO VEICULO(ano, marca_id, modelo, preco, is_vendido) VALUES (2020, 3, 'I30', 73000, true);
+INSERT INTO VEICULO(ano, marca_id, modelo, preco, is_vendido) VALUES (2020, 3, 'I30', 73000, false);
+INSERT INTO VEICULO(ano, marca_id, modelo, preco, is_vendido) VALUES (2018, 2, 'A3', 52000, true);
 
 --Insert para Usuários
 INSERT INTO TB_USUARIO (ds_email, ds_nome, ds_senha) VALUES ('almeidalima.igor@gmail.com', 'Igor de Almeida Lima', '$2a$10$Gn03nA57itFW1RqrQCOZPON5b2L296pX0HBcVwpuHiyyD7qBpph/C');
--- src/test/java/br/com/caelum/carangobom/controller/VeiculoControllerTest.java
+++ src/test/java/br/com/caelum/carangobom/controller/VeiculoControllerTest.java
@@ -12,22 +12,40 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.google.common.net.HttpHeaders;
+
+import br.com.caelum.carangobom.domain.Marca;
 import br.com.caelum.carangobom.domain.Veiculo;
+import br.com.caelum.carangobom.form.LoginForm;
+import br.com.caelum.carangobom.form.TokenForm;
 import br.com.caelum.carangobom.form.VeiculoForm;
 import br.com.caelum.carangobom.repository.VeiculoRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class VeiculoControllerTest {
 	
 	@Autowired
     private VeiculoController veiculoController;
+	
+	@Autowired
+	private AuthController authController;
+	
+	@Autowired
+	private MockMvc mockMvc;
 
     @Mock
     private VeiculoRepository veiculoRepository;
@@ -38,7 +56,19 @@ class VeiculoControllerTest {
         openMocks(this);
 
     }
-
+    
+    public String getTokenFromUser() {
+		LoginForm login = new LoginForm();
+		login.setEmail("almeidalima.igor@gmail.com");
+		login.setSenha("123456");
+		
+		ResponseEntity<TokenForm> token = authController.authentication(login);
+		
+		return token.getBody().getToken();
+	}
+    
+    
+    // Lista Veiculos Se Existir
     @Test
     void deveRetornarListaQuandoHouverResultadosVeiculos() {
     	
@@ -52,7 +82,22 @@ class VeiculoControllerTest {
         assertEquals(veiculos, resultado);
     }
     
+    
+    // Veiculos Vendidos Se Existir
+    @Test
+    void deveRetornarListaQuandoHouverResultadosVeiculosVendidos() {
+    	
+    	List<Veiculo> veiculos = new ArrayList<Veiculo>();
+    	veiculos.add(new Veiculo());
 
+        when(veiculoRepository.findByIdVedido())
+            .thenReturn(veiculos);
+
+        List<Veiculo> resultado = veiculoRepository.findByIdVedido();
+        assertEquals(veiculos, resultado);
+    }
+    
+    // Veiculos Por Id
     @Test
     void deveRetornarVeiculoPeloId() {
     	Integer id = 1;
@@ -60,9 +105,16 @@ class VeiculoControllerTest {
         assertEquals(HttpStatus.OK, resposta.getStatusCode());
     }
     
+    // Listar Todos Veiculos
     @Test
     void deveRetornarTodosVeiculo() {
         ResponseEntity<List<VeiculoForm>> resposta = veiculoController.lista();
+        assertEquals(HttpStatus.OK, resposta.getStatusCode());
+    }
+    
+    @Test
+    void deveRetornarTodosVeiculoVendido() {
+        ResponseEntity<List<VeiculoForm>> resposta = veiculoController.vendido();
         assertEquals(HttpStatus.OK, resposta.getStatusCode());
     }
     
@@ -86,4 +138,43 @@ class VeiculoControllerTest {
         ResponseEntity<VeiculoForm> resposta = veiculoController.deleta(10L);
         assertEquals(HttpStatus.NOT_FOUND, resposta.getStatusCode());
     }
+    
+    @Test
+    void deveResponderCreatedELocationQuandoCadastrarVeiculos() throws Exception {
+    	
+    	String json = "{\"ano\": \"2020\", \"modelo\": \"Corolla\", \"preco\": \"73000\", \"isVendido\": \"true\", \"marca\": { \"id\": \"1\"} }";
+    	
+    	MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.post("/veiculo")
+                					  .header(HttpHeaders.AUTHORIZATION, "Bearer " + getTokenFromUser())
+                                      .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                      .accept(MediaType.APPLICATION_JSON)
+                                      .characterEncoding("UTF-8")
+                                      .content(json);
+    			this.mockMvc.perform(builder)
+    		    .andExpect(MockMvcResultMatchers.status().is(201));
+
+    }
+    
+    @Test
+    void deveAlterarVeiculos() throws Exception {
+    	
+    	String json = "{\"ano\": \"2020\", \"modelo\": \"Corolla\", \"preco\": \"73000\", \"isVendido\": \"true\", \"marca\": { \"id\": \"1\"} }";
+    	
+    	Integer id = 2;
+    	
+    	MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.put("/veiculo/" + id)
+                					  .header(HttpHeaders.AUTHORIZATION, "Bearer " + getTokenFromUser())
+                                      .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                      .accept(MediaType.APPLICATION_JSON)
+                                      .characterEncoding("UTF-8")
+                                      .content(json);
+    			this.mockMvc.perform(builder)
+    		    .andExpect(MockMvcResultMatchers.status().is(200));
+
+    }
+    
+    
+
 }
