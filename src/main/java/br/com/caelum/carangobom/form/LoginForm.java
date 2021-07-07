c30e64a41599796512a439dd03a68bdd1c248b81
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-07-07 16:32:06 -0300

    Implementando segurança no springAdmin

--- src/main/java/br/com/caelum/carangobom/form/LoginForm.java
+++ src/main/java/br/com/caelum/carangobom/form/LoginForm.java
@@ -2,26 +2,15 @@ package br.com.caelum.carangobom.form;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginForm {
 
     private String email;
     private String senha;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 
 	public UsernamePasswordAuthenticationToken converter() {
 		return new UsernamePasswordAuthenticationToken(email, senha);
