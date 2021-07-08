7da762705ab45a7f98f1d0399d3236c25937cf6d
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-07-08 10:32:52 -0300

    Fix cors

--- src/main/java/br/com/caelum/carangobom/config/security/ActuatorSecurityConfigurations.java
+++ src/main/java/br/com/caelum/carangobom/config/security/ActuatorSecurityConfigurations.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@Order(1)
+//@Order(1)
 public class ActuatorSecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Override
